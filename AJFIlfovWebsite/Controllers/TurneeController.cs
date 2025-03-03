515034728a08f3fb98ab7619b19a2d375350a78e
Eduard-Gabriel Cristea <eduardcr@playtika.com>
2025-03-03 21:58:54 +0200

    changes

---
+++ AJFIlfovWebsite/Controllers/TurneeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AJFIlfovWebsite.Controllers
+{
+    public class TurneeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
